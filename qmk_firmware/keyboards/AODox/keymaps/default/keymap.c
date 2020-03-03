34c47cf13eb1ad8e8225ce54074a95c6048a8cf1
Pete Adriano DeBiase <peter.adriano.debiase@gmail.com>
2020-03-03 03:15:28 -0800

    Add rough QWTY and QSYM layers

--- qmk_firmware/keyboards/AODox/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/AODox/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * │Ent/MDA│  Q  │  W  │  E  │  R  │  T  │ F14 │   │     │   │ CALC│  Y  │  U  │  I  │  O  │  P  │  F22  │
  * ├───────┼─────┼─────┼─────╆─────╅─────┤     │   ├─────┤   │     ├─────╆─────╅─────┼─────┼─────┼───────┤
  * │Tab/SYM│  A  │  S  │  D  │  F  │  G  ├─────┤   │     │   ├─────┤  H  │  ←  │  ↓  │  ↑  │  →  │   "   │
- * ├───────┼─────┼─────┼─────╄─────╃─────┤Esc/ │   └─────┘   │  K /├─────╄─────╃─────┼─────┼─────┼───────┤
+ * ├───────┼─────┼─────┼─────╄─────╃─────┤Esc/ │   └─────┘   │  K/ ├─────╄─────╃─────┼─────┼─────┼───────┤
  * │  F15  │  Z  │  X  │  C  │  V  │  B  │LCAG ├─────┐ ┌─────┤ LCAG│  N  │  M  │  ,  │  .  │  ?  │  F23  │
  * └┬──────┼─────┼─────┼─────┼─────┼─────┼─────┤PgUp │ │ PgDn├─────┼─────┼─────┼─────┼─────┼─────┼──────┬┘
  *  │ LGui │Dl/Al│Bp/Sf│Sp/Cl│ F16 │ F17 │ F18 ├─────┤ ├─────┤ F19 │ F20 │ F21 │Sp/Cl│Bp/Sf│Is/Al│ PtSc │
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LGUI,         ALT_T(KC_DELT), SFT_T(KC_BSPC), CTL_T(KC_SPC), KC_F16, KC_F17, KC_F18,         ALL_T(KC_HOME),
 
     // Center keys
-    KC_NO,
-    KC_NO,
+    TG(QWTY),
+    KC_TRNS,
 
     // Right hand
                       KC_F13,    KC_F6,   KC_F7,    KC_F8,         KC_F9,          KC_F10,     KC_GRV,
@@ -103,50 +103,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty layout
  * ┌───────┬─────┬─────┬─────┬─────┬─────┬─────┐             ┌─────┬─────┬─────┬─────┬─────┬─────┬───────┐
- * │       │     │     │     │     │     │     │ Center keys │     │     │     │     │     │     │       │
+ * │   -   │ F1  │ F2  │ F3  │ F4  │ F5  │     │ Center keys │ F13 │  F6 │  F7 │  F8 │  F9 │ F10 │   ~   │
  * ├───────┼─────┼─────┼─────┼─────┼─────┼─────┤   ┌─────┐   ├─────┼─────┼─────┼─────┼─────┼─────┼───────┤
- * │       │     │     │     │     │     │     │   │     │   │     │     │     │     │     │     │       │
+ * │Ent/MDA│  Q  │  W  │  E  │  R  │  T  │ F14 │   │     │   │ CALC│  Y  │  U  │  I  │  O  │  P  │   \   │
  * ├───────┼─────┼─────┼─────╆─────╅─────┤     │   ├─────┤   │     ├─────╆─────╅─────┼─────┼─────┼───────┤
- * │       │     │     │     │     │     ├─────┤   │     │   ├─────┤     │     │     │     │     │       │
- * ├───────┼─────┼─────┼─────╄─────╃─────┤     │   └─────┘   │     ├─────╄─────╃─────┼─────┼─────┼───────┤
- * │       │     │     │     │     │     │     ├─────┐ ┌─────┤     │     │     │     │     │     │       │
- * └┬──────┼─────┼─────┼─────┼─────┼─────┼─────┤     │ │     ├─────┼─────┼─────┼─────┼─────┼─────┼──────┬┘
- *  │      │     │     │     │     │     │     ├─────┤ ├─────┤     │     │     │     │     │     │      │
- *  └──────┴─────┴─────┴─────┤     │     │     │     │ │     │     │     │     ├─────┴─────┴─────┴──────┘
+ * │Tab/SYM│  A  │  S  │  D  │  F  │  G  ├─────┤   │ Ins │   ├─────┤  H  │  J  │  K  │  L  │  ;  │ "/SYM │
+ * ├───────┼─────┼─────┼─────╄─────╃─────┤Esc/ │   └─────┘   │  K/ ├─────╄─────╃─────┼─────┼─────┼───────┤
+ * │ (/Sft │  Z  │  X  │  C  │  V  │  B  │LCAG ├─────┐ ┌─────┤ LCAG│  N  │  M  │  ,  │  .  │  ?  │ )/Sft │
+ * └┬──────┼─────┼─────┼─────┼─────┼─────┼─────┤PgUp │ │ PgDn├─────┼─────┼─────┼─────┼─────┼─────┼──────┬┘
+ *  │ LGui │Dl/Al│Bp/Sf│Sp/Cl│ F16 │ Bsp │ Dlt ├─────┤ ├─────┤ F19 │ Spc │  ←  │ ↓/Cl│ ↑/Sf│ →/Al│ PtSc │
+ *  └──────┴─────┴─────┴─────┤     │     │     │Hm/Hp│ │Ed/Mh│     │     │     ├─────┴─────┴─────┴──────┘
  *                           └─────┴─────┴─────┴─────┘ └─────┴─────┴─────┴─────┘
                   Left hand                                                     Right hand
  */
 [QWTY] = LAYOUT(
     // Left hand
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_MINS,              KC_F1,          KC_F2,         KC_F3,     KC_F4,  KC_F5,   KC_TRNS,
+    LT(MDIA, KC_ENT),     KC_Q,           KC_W,          KC_E,      KC_R,   KC_T,    KC_F14,
+    LT(QSYM, KC_TAB),     KC_A,           KC_S,          KC_D,      KC_F,   KC_G,
+    KC_LSPO,              KC_Z,           KC_X,          KC_C,      KC_V,   KC_B,    LCAG_T(KC_ESC), KC_PGUP,
+    KC_LGUI,         ALT_T(KC_DELT), SFT_T(KC_BSPC), CTL_T(KC_SPC), KC_F16, KC_BSPC, KC_DELT,        ALL_T(KC_HOME),
 
     // Center keys
     KC_TRNS,
-    KC_TRNS,
+    KC_INS,
 
     // Right hand
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                      KC_F13,    KC_F6,   KC_F7,    KC_F8,          KC_F9,        KC_F10,       KC_GRV,
+                      KC_CALC,   KC_Y,    KC_U,     KC_I,           KC_O,          KC_P,        KC_BSLS,
+                                 KC_H,    KC_J,     KC_K,           KC_L,         KC_SCLN,      LT(QSYM, KC_QUOT),
+    KC_PGDN,       LCAG_T(KC_K), KC_N,    KC_M,    KC_COMM,         KC_DOT,       KC_SLSH,      KC_RSPC,
+    MEH_T(KC_END),    KC_F19,    KC_SPC, KC_LEFT, CTL_T(KC_DOWN), SFT_T(KC_UP), ALT_T(KC_RGHT), KC_PSCR
 ),
 
 /* Qwerty symbols
  * ┌───────┬─────┬─────┬─────┬─────┬─────┬─────┐             ┌─────┬─────┬─────┬─────┬─────┬─────┬───────┐
  * │       │     │     │     │     │     │     │ Center keys │     │     │     │     │     │     │       │
  * ├───────┼─────┼─────┼─────┼─────┼─────┼─────┤   ┌─────┐   ├─────┼─────┼─────┼─────┼─────┼─────┼───────┤
- * │       │     │     │     │     │     │     │   │     │   │     │     │     │     │     │     │       │
+ * │       │  #  │  @  │  {  │  }  │  |  │     │   │     │   │     │  -  │  7  │  8  │  9  │  *  │   \   │
  * ├───────┼─────┼─────┼─────╆─────╅─────┤     │   ├─────┤   │     ├─────╆─────╅─────┼─────┼─────┼───────┤
- * │       │     │     │     │     │     ├─────┤   │     │   ├─────┤     │     │     │     │     │       │
+ * │       │  !  │  $  │  [  │  ]  │  `  ├─────┤   │     │   ├─────┤  :  │  4  │  5  │  6  │  +  │   -   │
  * ├───────┼─────┼─────┼─────╄─────╃─────┤     │   └─────┘   │     ├─────╄─────╃─────┼─────┼─────┼───────┤
- * │       │     │     │     │     │     │     ├─────┐ ┌─────┤     │     │     │     │     │     │       │
+ * │       │  %  │  ^  │  &  │  =  │     │     ├─────┐ ┌─────┤     │  ,  │  1  │  2  │  3  │  /  │       │
  * └┬──────┼─────┼─────┼─────┼─────┼─────┼─────┤     │ │     ├─────┼─────┼─────┼─────┼─────┼─────┼──────┬┘
- *  │      │     │     │     │     │     │     ├─────┤ ├─────┤     │     │     │     │     │     │      │
+ *  │      │     │     │     │     │  .  │     ├─────┤ ├─────┤     │     │  0  │  ↓  │  ↑  │     │      │
  *  └──────┴─────┴─────┴─────┤     │     │     │     │ │     │     │     │     ├─────┴─────┴─────┴──────┘
  *                           └─────┴─────┴─────┴─────┘ └─────┴─────┴─────┴─────┘
                   Left hand                                                     Right hand
@@ -154,10 +154,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QSYM] = LAYOUT(
     // Left hand
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_HASH,  KC_AT,  KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
+    KC_TRNS, KC_EXLM, KC_DLR,  KC_LBRC, KC_RBRC, KC_G,
+    KC_TRNS, KC_PERC, KC_CIRC, KC_AMPR, KC_EQL,  KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PDOT, KC_TRNS, KC_TRNS,
 
     // Center keys
     KC_TRNS,
@@ -165,10 +165,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Right hand
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+             KC_TRNS, KC_MINS,   KC_7,    KC_8,    KC_9,  KC_ASTR, KC_BSLS,
+                      KC_COLN,   KC_4,    KC_5,    KC_6,  KC_PPLS, KC_MINS,
+    KC_TRNS, KC_TRNS, KC_COMM,   KC_1,    KC_2,    KC_3,  KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,   KC_0,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Media, power, and RESET
