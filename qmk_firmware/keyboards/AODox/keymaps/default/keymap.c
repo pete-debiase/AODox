daa8b01420abd56510a2791ba31f800e8e0e8ac7
Pete Adriano DeBiase <peter.adriano.debiase@gmail.com>
2021-12-11 17:21:02 -0800

    Several minor QOL keymap tweaks

--- qmk_firmware/keyboards/AODox/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/AODox/keymaps/default/keymap.c
@@ -27,19 +27,19 @@ enum AODox_layers {
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-    QMK_VRN,
-    QMKBEST = SAFE_RANGE,
+    QMK_VRN = SAFE_RANGE,
+    QMKBEST,
     QMKURL
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Steno
+/*Steno
  * ┌───────┬─────┬─────┬─────┬─────┬─────┬─────┐             ┌─────┬─────┬─────┬─────┬─────┬─────┬───────┐
- * │   -   │ F1  │ F2  │ F3  │ F4  │ F5  │tQWTY│ Center keys │ F13 │  F6 │  F7 │  F8 │  F9 │ F10 │   ~   │
+ * │   -   │ F1  │ F2  │ F3  │ F4  │ F5  │     │ Center keys │ F13 │  F6 │  F7 │  F8 │  F9 │ F10 │   ~   │
  * ├───────┼─────┼─────┼─────┼─────┼─────┼─────┤   ┌─────┐   ├─────┼─────┼─────┼─────┼─────┼─────┼───────┤
  * │Ent/MDA│  Q  │  W  │  E  │  R  │  T  │ F14 │   │tQWTY│   │     │  Y  │  U  │  I  │  O  │  P  │  F22  │
  * ├───────┼─────┼─────┼─────╆─────╅─────┤     │   ├─────┤   │     ├─────╆─────╅─────┼─────┼─────┼───────┤
- * │Tab/SYM│  A  │  S  │  D  │  F  │  G  ├─────┤   │tQWTY│   ├─────┤  H  │  ←  │  ↓  │  ↑  │  →  │   "   │
+ * │Tab/SYM│  A  │  S  │  D  │  F  │  G  ├─────┤   │ Esc │   ├─────┤  H  │  ←  │  ↓  │  ↑  │  →  │   "   │
  * ├───────┼─────┼─────┼─────╄─────╃─────┤Esc/ │   └─────┘   │  K/ ├─────╄─────╃─────┼─────┼─────┼───────┤
  * │  F15  │  Z  │  X  │  C  │  V  │  B  │LCAG ├─────┐ ┌─────┤ LCAG│  N  │  M  │  ,  │  .  │  ?  │  F23  │
  * └┬──────┼─────┼─────┼─────┼─────┼─────┼─────┤PgUp │ │ PgDn├─────┼─────┼─────┼─────┼─────┼─────┼──────┬┘
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [STNO] = LAYOUT(
     // Left hand
-    KC_MINS,              KC_F1,          KC_F2,         KC_F3,     KC_F4,  KC_F5,  TG(QWTY),
+    KC_MINS,              KC_F1,          KC_F2,         KC_F3,     KC_F4,  KC_F5,  KC_NO,
     LT(MDIA, KC_ENT),     KC_Q,           KC_W,          KC_E,      KC_R,   KC_T,   KC_F14,
     LT(SNPD, KC_TAB),     KC_A,           KC_S,          KC_D,      KC_F,   KC_G,
     KC_F15,               KC_Z,           KC_X,          KC_C,      KC_V,   KC_B,   LCAG_T(KC_ESC), KC_PGUP,
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├───────┼─────┼─────┼─────┼─────┼─────┼─────┤   ┌─────┐   ├─────┼─────┼─────┼─────┼─────┼─────┼───────┤
  * │       │     │     │     │     │     │     │   │     │   │ CALC│  :  │  7  │  8  │  9  │  *  │   \   │
  * ├───────┼─────┼─────┼─────╆─────╅─────┤     │   ├─────┤   │     ├─────╆─────╅─────┼─────┼─────┼───────┤
- * │       │     │     │     │     │     ├─────┤   │     │   ├─────┤  .  │  4  │  5  │  6  │  +  │   -   │
+ * │       │     │     │     │     │     ├─────┤   │     │   ├─────┤  .  │  4  │  5  │  6  │  -  │   +   │
  * ├───────┼─────┼─────┼─────╄─────╃─────┤     │   └─────┘   │     ├─────╄─────╃─────┼─────┼─────┼───────┤
  * │       │     │     │     │     │     │     ├─────┐ ┌─────┤     │  ,  │  1  │  2  │  3  │  /  │       │
  * └┬──────┼─────┼─────┼─────┼─────┼─────┼─────┤     │ │     ├─────┼─────┼─────┼─────┼─────┼─────┼──────┬┘
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Right hand
              KC_NLCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,  KC_F12,
              KC_CALC, KC_COLN,  KC_7,    KC_8,    KC_9,   KC_PAST, KC_BSLS,
-                      KC_PDOT,  KC_4,    KC_5,    KC_6,   KC_PPLS, KC_PMNS,
+                      KC_PDOT,  KC_4,    KC_5,    KC_6,   KC_PMNS, KC_PPLS,
     KC_TRNS, KC_TRNS, KC_TRNS,  KC_1,    KC_2,    KC_3,   KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS,  KC_0,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
@@ -183,8 +183,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├───────┼─────┼─────┼─────╄─────╃─────┤     │   └─────┘   │     ├─────╄─────╃─────┼─────┼─────┼───────┤
  * │       │     │     │     │Mute │     │     ├─────┐ ┌─────┤     │     │     │     │     │     │       │
  * └┬──────┼─────┼─────┼─────┼─────┼─────┼─────┤     │ │     ├─────┼─────┼─────┼─────┼─────┼─────┼──────┬┘
- *  │      │     │     │     │Play/│     │     ├─────┤ ├─────┤     │     │     │     │     │     │      │
- *  └──────┴─────┴─────┴─────┤Pause│     │     │     │ │QMKVN│     │     │     ├─────┴─────┴─────┴──────┘
+ *  │      │     │     │     │     │Play/│     ├─────┤ ├─────┤     │     │     │     │     │     │      │
+ *  └──────┴─────┴─────┴─────┤     │Pause│     │     │ │QMKVN│     │     │     ├─────┴─────┴─────┴──────┘
  *                           └─────┴─────┴─────┴─────┘ └─────┴─────┴─────┴─────┘
                   Left hand                                                     Right hand
  */
