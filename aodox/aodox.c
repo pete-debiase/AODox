5dea8c646c200ba961221e5237d3e681d8348845
Pete Adriano DeBiase <peter.adriano.debiase@gmail.com>
2021-12-11 23:05:56 -0800

    Tweak directory capitalization
    Seemed to be related to an issue that prevented the firmware from building. Seems dumb, but it really seems like that's the case.

--- aodox/aodox.c
+++ aodox/aodox.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "AODox.h"
+#include "aodox.h"
 
 // Optional override functions below.
 // You can leave any or all of these undefined.
